2d18a5fca74be2a9c782c012a47127a961afe4e1
Maatijaa <matijaprox@gmail.com>
2024-10-03 17:37:31 +0200

    Add project files.

---
+++ OpenGLEngine/OGame.h
@@ -0,0 +1,19 @@
+# pragma once
+
+#include "OWindow.h"
+
+class OWindow;
+class OGame
+{
+public:
+	OGame();
+	~OGame();
+
+	void run();
+	void quit();
+protected:
+	bool m_isRunning = true;
+	OWindow* m_display = nullptr;
+
+};
+
---
+++ OpenGLEngine/OWindow.cpp
@@ -0,0 +1,28 @@
+#include "OWindow.h"
+#include "Windows.h"
+#include <assert.h>
+
+OWindow::OWindow()
+{
+    WNDCLASSEX wc = {};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpszClassName = L"OGL3DWindow";
+    wc.lpfnWndProc = DefWindowProc;
+
+    assert(RegisterClassEx(&wc));
+
+    RECT rc = { 0,0,1024,768 };
+    AdjustWindowRect(&rc, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU, false);
+
+    m_handle = CreateWindowEx(NULL, L"OGL3DWindow", L"Firebite | OpenGL 3D Game Engine", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+        CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, NULL, NULL);
+
+    assert(m_handle);
+
+    ShowWindow((HWND)m_handle, SW_SHOW);
+    UpdateWindow((HWND)m_handle);
+}
+
+OWindow::~OWindow()
+{
+}
